afa23c0265370a6ffa2df73a60cdfd1fa40ffd3f
Fiona Love <fl299@medschl.cam.ac.uk>
2021-08-16 11:59:07 +0100

    Adding functionality to rename new or edited files
    Includes optional parameter and behaviour to replace uuid only with original filename
    
    Now imports stringr for matching string against vector of patterns

--- R/blinder.R
+++ R/blinder.R
@@ -76,9 +76,12 @@ blind <- function(destination = NULL, input = NULL, key.name = "key.csv", key.di
 #' @param target The directory containing blinded files to restore.  If this is not provided, the user will be prompted for a directory.
 #' @param key.name Name of the CSV file containing the key of original and cryptic names.  Defaults to \code{key.csv}.
 #' @param key.dir Directory where the CSV key is saved.  If not provided, the target directory is assumed.
+#' @param rename.new Whether or not new files containing the uuids should be renamed.  This can be helpful if you've generated analysis files based on the blinded file names.  The uuid will be replaced with the original file name, with the rest of the new file name unchanged.  This may also be required if you've moved the files to a new directory after blinding.
 #'
 #' @export
-unblind <- function(target = NULL, key.name = "key.csv", key.dir = NULL){
+#'
+#' importFrom stringr str_detect
+unblind <- function(target = NULL, key.name = "key.csv", key.dir = NULL, rename.new = TRUE){
 
   filesep = .Platform$file.sep
   if(missing(target)){
@@ -93,12 +96,27 @@ unblind <- function(target = NULL, key.name = "key.csv", key.dir = NULL){
   if(missing(key.dir)){ key.dir = target }
 
   key = read.csv(file = paste(key.dir, key.name, sep = filesep), stringsAsFactors = F) #TODO - account for multiple
-  sapply(1:nrow(key),
-         function(i){
-           file.rename(from = key$new_path[i], to = paste(target, key$original[i], sep = filesep));
-         }
-  )
 
+  if(rename.new == TRUE){
+    files = list.files(target)
+    sapply(files, function(f){
+        uuid_matches = subset(key$uuid, str_detect(f, key$uuid));
+        new_name = f;
+        for(m in uuid_matches){
+          #two passes to avoid duplicating file extensions
+          new_name = sub(key[key$uuid == m, "new"], key[key$uuid == m, "original"], new_name);#first pass to check for 'new' - uuid with file extension, should be original file
+          new_name = sub(m, key[key$uuid == m, "original"], new_name);#second pass to check for uuid in file name - most likely new/changed
+        }
+        file.rename(from = paste(target, f, sep = filesep), to = paste(target, new_name, sep = filesep));
+      })
+  }
+  else{
+    sapply(1:nrow(key),
+           function(i){
+             file.rename(from = key$new_path[i], to = paste(target, key$original[i], sep = filesep));
+           }
+    )
+  }
 
 }
 
